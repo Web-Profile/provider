4fea759f46c8a50f013b0275629e24bb3585f59f
Daniel Buchner <danieljb2@gmail.com>
2015-09-23 12:46:58 -0700

    updates to app

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
 
 xtag.addEvent(document.getElementById('register_handler'), 'tap', function(event){
-  navigator.webProfile.registerProvider('MS Profile Service');
+  navigator.webProfile.registerProvider('Web Profile Service');
 });
